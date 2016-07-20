80d82703b0bb28b719b362a54fc126c222795b3a
tgrosh <tgrosh@yahoo.com>
2016-07-19 23:47:38 -0400

    Artillery Slider for showing slider value

---
+++ Assets/Scripts/ArtillerySlider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ArtillerySlider : MonoBehaviour {
+    public Text text;
+    public string suffix;
+
+    private AudioSource uiAudioSource;
+    private Slider slider;    
+
+    // Use this for initialization
+    void Start () {
+        slider = GetComponent<Slider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = slider.value + suffix;
+	}
+
+    public void SliderValueChanged(float value)
+    {
+        
+    }
+}
