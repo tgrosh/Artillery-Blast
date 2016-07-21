6ab2a3ecd87b7f22de8116090bfa2ca0e1ec1378
tgrosh <tgrosh@yahoo.com>
2016-07-21 17:45:46 -0400

    Tank is now Explodable

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -1,53 +1,26 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class CannonBall : MonoBehaviour {
-    public float explosionRadius;
-    public Detonator explosionPrefab;
-    public Detonator explosionGroundPrefab;
-    public AudioClip explosionAudioClip;
-
-	// Use this for initialization
-	void Start () {
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.name.Contains("Tank"))
-        {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-            foreach (Collider collider in colliders)
-            {
-                Rigidbody body = collider.GetComponent<Rigidbody>();
-
-                if (body != null)
-                {
-                    if (collider.gameObject.name != "Ground")
-                    {
-                        body.isKinematic = false;
-                    }
-                }
-            }
-            
-            Explode(explosionPrefab, 1f);
-        }
+        Explode(col.gameObject);
     }
 
     void OnCollisionEnter(Collision col)
     {
-        Explode(explosionGroundPrefab, .5f);
+        Explode(col.gameObject);
     }
 
-    void Explode(Detonator detonatorPrefab, float volume)
+    void Explode(GameObject collider)
     {
-        AudioSource.PlayClipAtPoint(explosionAudioClip, transform.position, volume);
-        Instantiate(detonatorPrefab, transform.position, Quaternion.identity);
+        Explodable explodable = collider.GetComponentInParent<Explodable>();
+
+        if (explodable != null)
+        {
+            explodable.Explode();
+        }
+
+        gameObject.GetComponent<Explodable>().Explode();
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Explodable.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explodable : MonoBehaviour {
+    public Detonator detonatorPrefab;
+    public AudioClip explosionAudioClip;
+    [Range(0, 1)]
+    public float explosionVolume;
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Explode()
+    {
+        Collider[] colliders = transform.GetComponentsInChildren<Collider>(); // Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody body = collider.GetComponent<Rigidbody>();
+
+            if (body != null)
+            {
+                body.isKinematic = false;
+            }
+        }
+
+        AudioSource.PlayClipAtPoint(explosionAudioClip, transform.position);
+        Instantiate(detonatorPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject, detonatorPrefab.destroyTime);
+    }
+}
