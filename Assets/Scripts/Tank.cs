44552890cf6f9404d1b8c09abbe01c05bc90c5b8
tgrosh <tgrosh@yahoo.com>
2016-07-22 18:25:04 -0400

    Tank Color

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour {
+    public Color color;
+
+	// Use this for initialization
+	void Start () {
+	    foreach (Transform childTransform in transform)
+        {
+            if (childTransform.CompareTag("PlayerColor"))
+            {
+                childTransform.gameObject.GetComponent<MeshRenderer>().materials[0].SetColor("_Color", color);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
