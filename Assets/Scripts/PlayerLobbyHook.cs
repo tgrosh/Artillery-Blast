6fd940542a0e5668af08da62facda51906cd688e
tgrosh <tgrosh@yahoo.com>
2016-12-30 18:31:26 -0500

    Multiplayer - Player Color

---
+++ Assets/Scripts/PlayerLobbyHook.cs
@@ -0,0 +1,26 @@
+﻿using Prototype.NetworkLobby;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerLobbyHook : LobbyHook {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        Tank tank = gamePlayer.GetComponent<Tank>();
+        tank.color = lobbyPlayer.GetComponent<LobbyPlayer>().playerColor;
+
+        base.OnLobbyServerSceneLoadedForPlayer(manager, lobbyPlayer, gamePlayer);
+    }
+}
--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -4,28 +4,32 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Tank : NetworkBehaviour {
+    [SyncVar]
     public Color color;
     public Cannon cannon;
 
 	// Use this for initialization
-	public void Start() {
-	    foreach (Transform childTransform in transform)
+	public void Start()
+    {
+        foreach (Transform childTransform in transform)
         {
             if (childTransform.CompareTag("TankColor"))
             {
                 childTransform.gameObject.GetComponent<MeshRenderer>().materials[0].SetColor("_Color", color);
             }
         }
-	}
+    }
 
     public override void OnStartLocalPlayer()
     {
         cannon.angleSlider = GameObject.Find("AngleSlider").GetComponent<Slider>();
         cannon.powerSlider = GameObject.Find("PowerSlider").GetComponent<Slider>();
+
+        base.OnStartLocalPlayer();
     }
-	
-	// Update is called once per frame
-	void Update () {
+    
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
