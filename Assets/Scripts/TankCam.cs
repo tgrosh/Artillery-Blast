e4cac37138dbb1315cddc50e7bb81fc305249c61
tgrosh <tgrosh@yahoo.com>
2017-02-08 22:58:04 -0500

    Camera Smoothing during Focus

--- Assets/Scripts/TankCam.cs
+++ Assets/Scripts/TankCam.cs
@@ -21,6 +21,7 @@ public class TankCam : FreeLookCam {
     Vector3 toTarget;
     Quaternion targetRotation;
     Quaternion targetPivotRotation;
+    float focusStartTime;
 
     // Use this for initialization
     protected override void Start () {
@@ -55,15 +56,19 @@ public class TankCam : FreeLookCam {
 
     void FixedUpdate()
     {
+        float t =  (Time.fixedTime - focusStartTime) / focusSpeed;
+        float focusStep = Mathf.SmoothStep(0, 1, t);
+        float zoomStep = (Mathf.Tan(focusStep) - (focusStep*.9f)) * 1.5f;
+
         if (focusTarget != null)
         {
             toTarget = focusTarget.position - transform.parent.position;
             targetRotation = Quaternion.LookRotation(toTarget, transform.parent.up);
             targetPivotRotation = Quaternion.identity;
-
+            
             if (transform.parent)
             {
-                transform.parent.rotation = Quaternion.Slerp(transform.parent.rotation, targetRotation, focusSpeed);
+                transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation, targetRotation, focusStep);
             }
             transform.localRotation = Quaternion.identity;
             m_Pivot.transform.localRotation = Quaternion.identity;
@@ -74,13 +79,13 @@ public class TankCam : FreeLookCam {
 
             if (transform.parent)
             {
-                transform.parent.rotation = Quaternion.Slerp(transform.parent.rotation, origDollyRotation, focusSpeed);
+                transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation, origDollyRotation, focusStep);
             }
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, focusSpeed);
-            m_Pivot.transform.localRotation = Quaternion.Slerp(m_Pivot.transform.localRotation, targetPivotRotation, focusSpeed);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, focusStep);
+            m_Pivot.transform.localRotation = Quaternion.Lerp(m_Pivot.transform.localRotation, targetPivotRotation, focusStep);
         }        
 
-        Camera.main.fieldOfView = Mathf.SmoothStep(Camera.main.fieldOfView, zoomInDistance, focusSpeed*2);            
+        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, zoomInDistance, zoomStep);            
         Time.timeScale = slowMoTimeScale;
         Time.fixedDeltaTime = deltaTimeRatio * Time.timeScale;        
     }   
@@ -104,6 +109,7 @@ public class TankCam : FreeLookCam {
     public void FocusOn(Transform target, float zoom, float timeScale)
     {
         focusTarget = target;
+        focusStartTime = Time.fixedTime;
         zoomInDistance = zoom;
         slowMoTimeScale = timeScale;
     }
@@ -111,12 +117,14 @@ public class TankCam : FreeLookCam {
     public void ClearFocus()
     {
         focusTarget = null;
+        focusStartTime = Time.fixedTime;
         ResetFocus();
     }
 
     public void ResetFocus()
     {
         slowMoTimeScale = 1f;
+        focusStartTime = Time.fixedTime;
         zoomInDistance = origCamFieldOfView;
-    }
+    }    
 }
